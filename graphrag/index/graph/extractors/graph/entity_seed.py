cb87b39cd21f40be8b272b5e886e56eba2678def
majidsh97 <sharghi.majid@gmail.com>
2024-11-12 00:42:07 +0100

    .

--- graphrag/index/graph/extractors/graph/entity_seed.py
+++ graphrag/index/graph/extractors/graph/entity_seed.py
@@ -68,7 +68,7 @@ def get_matched_entity(text,entity_seed):
 
 def pipeline(texts):
     entity_seed = get_entity_seed(texts)
-    #entity_seed = clean_entity_seed(entity_seed)
+    entity_seed = clean_entity_seed(entity_seed)
     entity_seed = pd.DataFrame(entity_seed.items(), columns=["entity", "count"])
     entity_seed.to_parquet("entity_seed.parquet")
     
